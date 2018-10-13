e4e3444fac5d6976a5ab8bbd21dfd64eafe40e1f
Cyclonecode <cyclonecode@gmail.com>
2018-10-13 19:13:53 +0200

    Add support to specify host/port during compile time

--- mysql-plugin/src/mysql-notification.c
+++ mysql-plugin/src/mysql-notification.c
@@ -21,9 +21,15 @@ typedef long long longlong;
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#ifndef SERVER_PORT
+#define SERVER_PORT 2048
+#endif
+#ifndef SERVER_ADDRESS
+#define SERVER_ADDRESS "127.0.0.1"
+#endif
+
 static pthread_mutex_t LOCK_hostname;
 static int _server = -1;
-static int running = 1;
 
 enum TRIGGER_TYPE {
     TRIGGER_CLOSE = 1,
@@ -32,47 +38,46 @@ enum TRIGGER_TYPE {
     TRIGGER_DELETE = 4
 };
 
-my_bool MySQLNotification_init(UDF_INIT *initid, 
-                                          UDF_ARGS *args,
-                                          char *message) {
+my_bool MySQLNotification_init(UDF_INIT *initid,
+                               UDF_ARGS *args,
+                               char *message) {
     // allocate memory here
     // longlong* i = malloc(sizeof(*i));
     //initid->ptr = (char*)i;
-    
     struct sockaddr_in remote, saddr;
 
     // check the arguments format
-    if(args->arg_count != 2) {
+    if (args->arg_count != 2) {
       strcpy(message, "MySQLNotification() requires two arguments");
       return 1;
     }
 
-    if(args->arg_type[0] != INT_RESULT || args->arg_type[1] != INT_RESULT) {
+    if (args->arg_type[0] != INT_RESULT || args->arg_type[1] != INT_RESULT) {
       strcpy(message, "MySQLNotification() requires two integers");
       return 1;
     }
-   
+
     // create a socket that will talk to our node server
     _server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(_server == -1) {
+    if (_server == -1) {
        return -1;
     }
-    
+
     // bind to local address
     memset(&saddr, 0, sizeof(saddr));
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(0);
     saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    if(bind(_server, (struct sockaddr*)&saddr, sizeof(saddr)) != 0) {
+    if (bind(_server, (struct sockaddr*)&saddr, sizeof(saddr)) != 0) {
         return -1;
     }
-    
+
     // connect to server
     memset(&remote, 0, sizeof(remote));
     remote.sin_family = AF_INET;
-    remote.sin_port = htons(2048);
-    remote.sin_addr.s_addr = inet_addr("127.0.0.1");
-    if(connect(_server, (struct sockaddr*)&remote, sizeof(remote)) != 0) {
+    remote.sin_port = htons(SERVER_PORT);
+    remote.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
+    if (connect(_server, (struct sockaddr*)&remote, sizeof(remote)) != 0) {
         strcpy(message, "Failed to connect to server on port 2048");
         return -1;
     }  
@@ -80,25 +85,25 @@ my_bool MySQLNotification_init(UDF_INIT *initid,
     return 0;
 }
 
-     
 void MySQLNotification_deinit(UDF_INIT *initid) {
     // free any allocated memory here
     //free((longlong*)initid->ptr);
     // close server socket
-    if(_server != -1) {
+    if (_server != -1) {
         close(_server);
     }
 }
 
-longlong MySQLNotification(UDF_INIT *initid, UDF_ARGS *args,
-                           char *is_null, char *error) {
-    
+longlong MySQLNotification(UDF_INIT *initid,
+                           UDF_ARGS *args,
+                           char *is_null,
+                           char *error) {
     char packet[512];
 
     // format a message containing id of row and type of change
-    sprintf(packet, "{\"id\":\"%lld\", \"type\":\"%lld\"}", *((longlong*)args->args[0]), *((longlong*)args->args[1]));   
-    
+    sprintf(packet, "{\"id\":\"%lld\", \"type\":\"%lld\"}", *((longlong*)args->args[0]), *((longlong*)args->args[1]));
+
     send(_server, packet, strlen(packet), 0);
-    
+
     return 0;
 }
