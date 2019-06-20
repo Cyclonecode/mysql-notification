967163ec61e9d32a0c3ea179f822a6ec43178820
Cyclonecode <cyclonecode@gmail.com>
2019-06-20 14:33:26 +0200

    Set default to development

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'development'
     },
     env_production: {
       NODE_ENV: 'production'
